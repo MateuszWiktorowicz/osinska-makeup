0e7deae419fc0d7fab30aa5723f0760b104ebf15
Mateusz Wiktorowicz <wiktorowicz.mat@gmail.com>
2025-05-17 13:09:09 +0200

    create nav config

---
+++ src/features/header/navigation.config.ts
@@ -0,0 +1,27 @@
+export const navigation = [
+  {
+    id: 1,
+    translationKey: "menu.home",
+    translationHref: "menu.homeHref",
+  },
+  {
+    id: 2,
+    translationKey: "menu.about",
+    translationHref: "menu.aboutHref",
+  },
+  {
+    id: 3,
+    translationKey: "menu.contact",
+    translationHref: "menu.contactHref",
+  },
+  {
+    id: 4,
+    translationKey: "menu.services",
+    translationHref: "menu.servicesHref",
+  },
+  {
+    id: 5,
+    translationKey: "menu.portfolio",
+    translationHref: "menu.portfolioHref",
+  },
+];
